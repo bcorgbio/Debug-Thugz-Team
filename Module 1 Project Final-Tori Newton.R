8b73bde1b52203b66207b2fc0d4ac9f172fc6e5c
torinewton <112513271+torinewton@users.noreply.github.com>
2022-09-09 10:55:43 -0400

    Add files via upload

---
+++ Module 1 Project Final-Tori Newton.R
@@ -0,0 +1,32 @@
+library(ggplot2)
+library(tidyverse)
+setwd("~/BIOL 3140 Projects Data")
+dat <- read.csv("scales.csv")
+sapply(dat,class)
+dim(dat)
+dat$species <- as.factor(dat$species)
+species <- levels(dat$species)
+species
+species.n<- dat %>%
+  group_by(species) %>%
+  summarise(n = n())
+species.n
+dat %>% 
+  count(species,specimen) %>%
+  print() %>%
+  count(species,name = "n.specimens")
+for(i in species){
+  p <- dat %>%
+    filter(species==i)%>%
+    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+pdf("species.quadrant.pdf")
+for(i in species){
+  p <- dat %>%
+    filter(species==i)%>%
+    ggplot()+geom_boxplot(aes(x=quadrant,y=N))+ggtitle(i)
+  print(p)
+}
+dev.off()
+list.files(pattern=".pdf")
