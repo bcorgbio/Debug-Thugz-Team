ab5573fcf86e4fa618130de22fdbfc1aaa0bafc7
Tori Newton <newtonvi@bc.edu>
2022-09-18 19:51:00 -0400

    Module 2 Commit

--- Module 2 Project Final - Tori Newton.R
+++ Module 2 Project Final - Tori Newton.R
@@ -4,7 +4,6 @@ pseed <- read_csv("pseed.fin.amps.csv")
 pseed.bl <- read_csv("pseed.lengths.csv")
 speeds <- read_csv("pseed.calibration.csv")
 
-
 pseed2 <- pseed%>%
   left_join(speeds,by=c("speed"="vol"))
 
