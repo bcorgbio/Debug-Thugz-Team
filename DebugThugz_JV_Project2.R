c88a41840df03de8497e27ddd2b732bef1ef29f5
vuju-bc <vuju@bc.edu>
2022-09-18 18:51:28 -0400

    commit #2

--- DebugThugz_JV_Project2.R
+++ DebugThugz_JV_Project2.R
@@ -5,13 +5,12 @@ pseed.bl <- read_csv("pseed.lengths.csv")
 speeds <- read_csv("pseed.calibration.csv")
 
 pseed2 <- pseed%>%
-  left_join(speeds,by=c("speed"="vol"))%>%
+  left_join(speeds,by=c("speed"="vol"))
 pseed2 <- pseed2%>%
-  left_join(pseed.bl,by="fish")%>%
+  left_join(pseed.bl,by="fish")
 pseed2 <- pseed2%>%
-  mutate(bl.s=cm.s/bl)%>%
+  mutate(bl.s=cm.s/bl)
 
-  
 pseed2 <- pseed2 %>%
   group_by(date,frame) %>%
   mutate(amp.sum=sum(amp.bl))
